ed103370d3125fd3e0d946ca299740e5646f3e90
aadhya-arora <aadhya270805@gmail.com>
2026-02-19 23:45:33 +0530

    built some logic

--- student_management/src/main/java/com/student_management/App.java
+++ student_management/src/main/java/com/student_management/App.java
@@ -1,5 +1,7 @@
 package com.student_management;
 
+import java.util.Scanner;
+
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
@@ -8,49 +10,87 @@ import org.hibernate.cfg.Configuration;
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        Configuration cfg=new  Configuration();
+public class App {
+    public static void main(String[] args) {
+        Configuration cfg = new Configuration();
         cfg.configure();
         cfg.addAnnotatedClass(Student.class);
         cfg.addAnnotatedClass(Library.class);
-        SessionFactory factory=cfg.buildSessionFactory();
-        Session session=factory.openSession();
-        Main_Student m=new Main_Student();
+        SessionFactory factory = cfg.buildSessionFactory();
+        Session session = factory.openSession();
+        Main_Student m = new Main_Student();
+        Main_Library l=new Main_Library();
+        Scanner sc = new Scanner(System.in);
         boolean running = true;
-        
-        while (running) 
+
+        System.out.println("Welcome to the Student Management System!");
+        System.out.println("Please select an option:");
+        System.out.println("1.Student Management");
+        System.out.println("2.Library Management");
+        int mainChoice=sc.nextInt();
+        sc.nextLine();
+
+
+        if(mainChoice==1)
         {
-            int choice = m.menu();
+            while (running) 
+            {
+                int choice = m.menu();
 
-            switch (choice) {
-                case 1:
-                    m.addStudent(session);
-                    break;
+                switch (choice) {
+                    case 1:
+                        m.addStudent(session);
+                        break;
 
-                case 2:
-                    m.viewStudents(session);
-                    break;
+                    case 2:
+                        m.viewStudents(session);
+                        break;
 
-                case 3:
-                    m.deleteStudent(session);
-                    break;
+                    case 3:
+                        m.deleteStudent(session);
+                        break;
 
-                case 4:
-                    m.updateStudent(session);
-                    break;
+                    case 4:
+                        m.updateStudent(session);
+                        break;
 
-                case 5:
-                    System.out.println("Exiting application...");
-                    running = false;
-                    break;
-                default:
-                    System.out.println("Invalid choice. Please try again.");
-                    break;
+                    case 5:
+                        System.out.println("Exiting application...");
+                        running = false;
+                        break;
+                    default:
+                        System.out.println("Invalid choice. Please try again.");
+                        break;
+                }
             }
         }
-        
+        else if(mainChoice==2)
+        {
+            while(running)
+            {
+                int  choice=l.menu();
+                switch(choice)
+                {
+                    case 1:
+                        l.addBook(session);
+                        break;
+                    case 2:
+                        l.viewBooks(session);
+                        break;
+                    case 3:
+                        l.deleteBook(session);
+                        break;
+                    case 4:
+                        l.updateBook(session);
+                        break;
+                    case 5:
+                        System.out.println("Exiting Library Management System...");
+                        running = false;
+                        break;
+                    default:
+                        System.out.println("Invalid choice. Please try again.");
+                }
+            }
+        }
     }
 }
--- student_management/src/main/java/com/student_management/Main_Library.java
+++ student_management/src/main/java/com/student_management/Main_Library.java
@@ -1,5 +1,60 @@
 package com.student_management;
 
+import java.util.Scanner;
+
+import org.hibernate.Session;
+import org.hibernate.Transaction;
 public class Main_Library {
     
+    public static int menu(){
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Welcome to the Library Management System!");
+        System.out.println("Please select an option:");
+        System.out.println("1.Add Book");
+        System.out.println("2.View Books");
+        System.out.println("3.Delete Book");
+        System.out.println("4.Update Book");
+        System.out.println("5.Exit");
+        int choice=sc.nextInt();
+        sc.nextLine();
+        return choice;
+    }
+
+    public static void addBook(Session session){
+        Transaction tx=null;
+        try {
+            Scanner sc=new Scanner(System.in);
+            System.out.println("Enter details of the book:");
+            System.out.print("Book Name:");
+            sc.nextLine();
+            String book_name=sc.nextLine();
+            System.out.print("Author:");
+            String author=sc.nextLine();
+            System.out.print("Genre:");
+            int genre=sc.nextInt();
+            Library l=new Library();
+            l.setBook_name(book_name);
+            l.setAuthor(author);
+            l.setGenre(genre);
+            tx=session.beginTransaction();
+            session.save(l);
+            tx.commit();
+            System.out.println("Book added successfully!");
+        } catch(Exception e){
+            if(tx!=null)
+            {
+                tx.rollback();
+            }
+            System.out.println("Error adding book");
+        }
+    }
+    public static void viewBooks(Session session){
+        
+    }
+    public static void deleteBook(Session session){
+        
+    }
+    public static void updateBook(Session session){
+        
+    }
 }
